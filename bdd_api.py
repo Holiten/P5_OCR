ad61a9bfe0d54a99c8f4f7d80267c79f1414d3f9
Bruno LE MARCHAND <hamiccc@gmail.com>
2020-02-10 14:22:03 +0100

    V 1.4 - 10/02/2020 - Optimisation & Suppression function 'erase_void'
    Signed-off-by: Bruno LE MARCHAND <hamiccc@gmail.com>

--- bdd_api.py
+++ bdd_api.py
@@ -1,102 +1,135 @@
-from constants import PAGE_NUMBER, CATEGORIES, CONFIG_TABLES, CONFIG_BDD
-import requests
-import json
-import mysql.connector
+"""
+Version 1.4 :  Optimisation & Suppression function 'erase_void'
+Version 1.3 : PEP convention
+Version 1.2 : Modifications & optimisation
+Version 1.1 : Pseudo-code
+Version 1.0 : Initialisation
+The following scripts manage the Open food fact's API and the database
+"""
 
-class bdd_api():
+import requests
+import mysql.connector
+from constants import PAGE_NUMBER, CATEGORIES, CONFIG_BDD, CONFIG_TABLES
+
+
+class BddApi():
+    """
+    Create bdd_api object to use data from Open food Fact API, create SQL BDD, create SQL Tables,
+    add data from api to bdd.
+    """
 
     def __init__(self):
         self.product_data = []
 
     def api_temp(self):
-        print("Veuillez patientez pendant le chargement...")
-        for page in range(0, PAGE_NUMBER):
+        """
+        Request Open food Fact API into variable with json and fill the empty list
+        """
+        print("Loading Open Food Fact API Data...")
+        for page in range(0, PAGE_NUMBER+1):
             for cat_key, cat_value in CATEGORIES.items():
                 if cat_key:
                     r_page = requests.get(cat_value + str(page + 1) + '.json')
                     r_page_json = r_page.json()
                     products_by_page = r_page_json[u'products']
 
-                    with open('products.json', 'w') as fp:
-                        json.dump(r_page_json, fp, indent=5)
-
                     for products in products_by_page:
-
                         name_cat = cat_key
                         name_product = products.get('product_name_fr', 'NULL')
                         score_product = products.get('nutriscore_grade', 'NULL')
                         url_product = products.get('url', 'Non renseigné')
-                        store_product = products.get('store', 'Non renseigné')
+                        store_product = products.get('stores', 'Non renseigné')
 
-                        self.product_data.append([name_cat, name_product, score_product, url_product, store_product])
-        print("Merci d'avoir patienter !")
+                        if name_product not in ["", "NULL"] and score_product not in["", "N"]:
+                            self.product_data.append([name_cat, name_product, score_product, url_product, store_product])
 
-    def create_db(self):
+        print("The data has been loaded successfully")
+    @staticmethod
+    def create_db():
+        """
+        Create a bdd named 'open_food_fact'
+        """
 
         cnx = mysql.connector.connect(**CONFIG_BDD)
         cursor = cnx.cursor()
 
         cursor.execute("DROP DATABASE IF EXISTS open_food_fact")
 
-        try :
+        try:
             cursor.execute(
                 "CREATE DATABASE IF NOT EXISTS `open_food_fact` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci"
             )
         except cnx.Error as err:
-            print("Erreur lors de la création de la base de donnée")
+            print(err)
             exit(1)
 
-    def create_tables(self):
+    @staticmethod
+    def create_tables():
+        """
+        Create tables 'products / categorie / user_data' in the bdd create previously by 'create_db(self)'
+        """
 
         cnx = mysql.connector.connect(**CONFIG_TABLES)
         cursor = cnx.cursor()
-        drop = "DROP TABLE IF EXISTS products, categories "
+        drop = "DROP TABLE IF EXISTS products, categories, user_data"
 
         prod_table = "CREATE TABLE IF NOT EXISTS products (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name_cat VARCHAR(50) NOT NULL, name_product VARCHAR(500) NOT NULL, score_product VARCHAR(1) NOT NULL, url_product VARCHAR(5000) NOT NULL, store_product VARCHAR(255) NOT NULL, INDEX(name_cat), CONSTRAINT blabla FOREIGN KEY(name_cat) REFERENCES categories(name_cat))"
 
         cat_table = "CREATE TABLE IF NOT EXISTS categories(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name_cat VARCHAR(50) NOT NULL, INDEX(name_cat))"
 
-        try :
+        user_data = "CREATE TABLE IF NOT EXISTS user_data(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, id_product INT NOT NULL, INDEX(id_product), CONSTRAINT id_product FOREIGN KEY(id_product) REFERENCES products(id))"
+
+        try:
             cursor.execute(drop)
             cursor.execute(cat_table)
             cursor.execute(prod_table)
-        except mysql.connector.Error as err :
+            cursor.execute(user_data)
+
+        except mysql.connector.Error as err:
             print(err)
             exit(1)
 
         cnx.close()
 
-    def send_cat_to_db(self):
+    @staticmethod
+    def send_cat_to_db():
+        """
+        Send data from list create with 'api_temp' to categories table
+        """
 
         cnx = mysql.connector.connect(**CONFIG_TABLES)
         cursor = cnx.cursor()
 
         insert_cat = "INSERT INTO categories (id, name_cat) VALUES (NULL, 'Snacks'), (NULL, 'Fromages'),(NULL, 'Boissons')"
 
-        cursor.execute(insert_cat)
-        cnx.commit()
-        cnx.close()
+        try:
+            cursor.execute(insert_cat)
+            cnx.commit()
+            cnx.close()
+
+        except mysql.connector.Error as err:
+            print(err)
+            exit(1)
 
     def send_product_to_db(self):
+        """
+        Send data from list create with 'api_temp' to products table
+        """
+
         data = self.product_data[:]
 
         cnx = mysql.connector.connect(**CONFIG_TABLES)
         cursor = cnx.cursor()
 
         insert_prod = "INSERT INTO products(id, name_cat, name_product, score_product, url_product, store_product) VALUES (NULL, %s, %s, %s, %s, %s)"
+        clean_prod = "DELETE FROM products WHERE name_product='NULL' OR name_product='' OR score_product='N'"
 
-        cursor.executemany(insert_prod, data)
-        cnx.commit()
-        cnx.close()
+        try:
+            cursor.executemany(insert_prod, data)
+            cursor.execute(clean_prod)
+            cnx.commit()
+            cnx.close()
 
-    def erase_void(self):
-        cnx = mysql.connector.connect(**CONFIG_TABLES)
-        cursor = cnx.cursor()
-
-        delete_score_null = "DELETE FROM products WHERE score_product='N'"
-        delete_name_null = "DELETE FROM products WHERE name_product='NULL' AND name_product=''"
-
-        cursor.execute(delete_score_null)
-        cursor.execute(delete_name_null)
-        cnx.commit()
-        cnx.close
+        except mysql.connector.Error as err:
+            print(err)
+            exit(1)
