cade0fb0781fbd63d21678e2b34285e72303b7e9
Bruno LE MARCHAND <hamiccc@gmail.com>
2020-03-09 15:44:54 +0100

    V 2.0 - 09/03/2020
    Signed-off-by: Bruno LE MARCHAND <hamiccc@gmail.com>

--- constants.py
+++ constants.py
@@ -1,45 +1,34 @@
-
+"""Constants file"""
 
 CATEGORIES = {'Yaourts': 'https://fr.openfoodfacts.org/categorie/yaourts/',
               'Fromages': 'https://fr.openfoodfacts.org/categorie/fromages/',
               'Boissons': 'https://fr.openfoodfacts.org/categorie/boissons/'}
 
 
-"""
-Get the size of a page
-"""
+
+"""Get the size of a page"""
 # PAGE_SIZE = url_r_json['page_size']
 
-"""
-Get the number of products
-"""
+"""Get the number of products"""
 # PRODUCT_NUMBER = url_r_json['count']
 
-"""
-Get the number of pages (((PRODUCT_NUMBER/PAGE_SIZE)+1)round)
-"""
+"""Get the number of pages (((PRODUCT_NUMBER/PAGE_SIZE)+1)round)"""
 PAGE_NUMBER = 5
 
-"""
-SQL connection parameter
-"""
+"""SQL connection parameter"""
 USER_NAME = 'root'
 USER_PASSWORD = ''
 USER_HOST = 'localhost'
-DB_NAME = 'open_food_fact'
+DB_NAME = "ocr_p5"
 PASSWORD_TYPE = 'mysql_native_password'
 
-"""
-BDD SQL configuration parameter
-"""
+"""BDD SQL configuration parameter"""
 CONFIG_BDD = {'user' : USER_NAME,
               'password' : USER_PASSWORD,
               'host' : USER_HOST,
               'auth_plugin' : PASSWORD_TYPE
               }
-"""
-BDD Tables SQL configuration parameter
-"""
+"""BDD Tables SQL configuration parameter"""
 CONFIG_TABLES = {'user' : USER_NAME,
                  'password' : USER_PASSWORD,
                  'host' : USER_HOST,
